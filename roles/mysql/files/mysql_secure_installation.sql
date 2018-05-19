541029aaa80a45ec34b08cc69619f1a6225031c7
set <serhii.dovhan@gmail.com>
2018-05-19 12:37:26 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   roles/mysql/files/mysql_secure_installation.sql
    	modified:   roles/mysql/tasks/main.yml
    	modified:   roles/mysql/templates/my.cnf.j2

--- roles/mysql/files/mysql_secure_installation.sql
+++ roles/mysql/files/mysql_secure_installation.sql
@@ -1,8 +1,8 @@
 -- remove_anonymous_users
-DELETE FROM mysql.user WHERE User='';
+-- DELETE FROM mysql.user WHERE User='';
 
 -- remove_remote_root
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
 -- Removing privileges on test database
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
